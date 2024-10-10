9d0c9e70d56f6fd7649cfd228efa26e03c356f6d
MartinDLaFuente <martindlafuentecoding@gmail.com>
2024-10-09 21:44:33 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Optional JavaScript for adding dynamic interactions in the future
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Portfolio Page Loaded Successfully");
+});
